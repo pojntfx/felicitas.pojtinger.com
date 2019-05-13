b514cd5188ce63a65332ec84b5e01483c39d960c
Felix Pojtinger <felix@pojtinger.com>
2019-05-14 00:25:21 +0200

    fix(site): Update dependencies and fix build

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,1 @@
-exports.onServiceWorkerUpdateFound = () => {
-  if (
-    window.confirm(
-      "This site has been updated with new data. Do you wish to reload the site to get the new data?"
-    )
-  ) {
-    window.location.reload(true);
-  }
-};
+exports.onServiceWorkerUpdateReady = () => window.location.reload(true);
