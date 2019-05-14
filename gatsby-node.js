a85b734736d4e4b8a0c11bf3093d703e2b5bfcf1
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-05-14 02:35:36 +0200

    build(site): Await async methods

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,64 +3,62 @@ exports.createPages = async ({ graphql, actions: { createPage } }) => {
 };
 
 createArticles = async (graphql, createPage) => {
-  try {
-    const { data } = await graphql(`
-      {
-        allMdx {
-          edges {
-            node {
-              parent {
-                ... on File {
-                  name
-                  absolutePath
-                }
+  const { data } = await graphql(`
+    {
+      allMdx {
+        edges {
+          node {
+            parent {
+              ... on File {
+                name
+                absolutePath
               }
-              frontmatter {
-                title
-                author
-                lastEdit
-                imgSrc
-                imgAlt
-                date
-                article
-              }
-              timeToRead
-              headings {
-                value
-                depth
-              }
-              excerpt
             }
+            frontmatter {
+              title
+              author
+              lastEdit
+              imgSrc
+              imgAlt
+              date
+              article
+            }
+            timeToRead
+            headings {
+              value
+              depth
+            }
+            excerpt
           }
         }
       }
-    `);
-    data.allMdx.edges.forEach(
-      ({ node: { parent, timeToRead, frontmatter, headings, excerpt } }) => {
-        createPage({
-          path: `/articles/${parent.name}`,
-          component: parent.absolutePath,
-          context: {
-            ...frontmatter,
-            title:
-              headings.filter(({ depth }) => depth === 1).length === 0
-                ? frontmatter.title
-                : headings.filter(({ depth }) => depth === 1)[0].value,
-            date: frontmatter.date
-              ? frontmatter.date
-              : parent.name
-                  .split("-")
-                  .filter((element, index) => (index < 3 ? element : null)) // Get the date from the article's filename, like with Jekyll
-                  .join("-"),
-            timeToRead,
-            excerpt
-          }
-        });
-      }
-    );
-  } catch (e) {
-    throw e;
-  }
+    }
+  `);
+  return data.allMdx.edges.forEach(
+    async ({
+      node: { parent, timeToRead, frontmatter, headings, excerpt }
+    }) => {
+      await createPage({
+        path: `/articles/${parent.name}`,
+        component: parent.absolutePath,
+        context: {
+          ...frontmatter,
+          title:
+            headings.filter(({ depth }) => depth === 1).length === 0
+              ? frontmatter.title
+              : headings.filter(({ depth }) => depth === 1)[0].value,
+          date: frontmatter.date
+            ? frontmatter.date
+            : parent.name
+                .split("-")
+                .filter((element, index) => (index < 3 ? element : null)) // Get the date from the article's filename, like with Jekyll
+                .join("-"),
+          timeToRead,
+          excerpt
+        }
+      });
+    }
+  );
 };
 
 exports.onCreateWebpackConfig = ({ actions }) => {
