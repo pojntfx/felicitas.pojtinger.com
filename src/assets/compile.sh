4f658a7cee0c5524d2e78014d23b24af086903fd
Felix Pojtinger <felix@pojtinger.com>
2019-07-09 23:58:35 +0200

    refactor(site): Disable circular favicon

--- src/assets/compile.sh
+++ src/assets/compile.sh
@@ -9,15 +9,16 @@ SIZE=152 && convert -resize ${SIZE}x${SIZE} icon.webp icon-${SIZE}x${SIZE}.webp 
 SIZE=192 && convert -resize ${SIZE}x${SIZE} icon.webp icon-${SIZE}x${SIZE}.webp &
 SIZE=384 && convert -resize ${SIZE}x${SIZE} icon.webp icon-${SIZE}x${SIZE}.webp &
 SIZE=512 && convert -resize ${SIZE}x${SIZE} icon.webp icon-${SIZE}x${SIZE}.webp &
-SIZE=512 && let HALFSIZE=${SIZE}/2 && convert icon.webp \
-	-gravity Center \
-	\( -size ${SIZE}x${SIZE} \
-	xc:Black \
-	-fill White \
-	-draw "circle ${HALFSIZE} ${HALFSIZE} ${HALFSIZE} 1" \
-	-alpha Copy \
-	\) -compose CopyOpacity -composite \
-	-trim icon-circle.webp &
+cp icon.webp icon-circle.webp &
+# SIZE=512 && let HALFSIZE=${SIZE}/2 && convert icon.webp \
+# 	-gravity Center \
+# 	\( -size ${SIZE}x${SIZE} \
+# 	xc:Black \
+# 	-fill White \
+# 	-draw "circle ${HALFSIZE} ${HALFSIZE} ${HALFSIZE} 1" \
+# 	-alpha Copy \
+# 	\) -compose CopyOpacity -composite \
+# 	-trim icon-circle.webp &
 rm *.png
 rm *.jpg
 rm *~
