95252b9d0ec380595d67b882556f04875a701ac3
Felix Pojtinger <felix@pojtinger.com>
2019-10-09 20:39:59 +0000

    build: Add Docker, Kubernetes, Helm and Skaffold configuration for personal-site

--- src/@types/mdx.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.mdx" {
-  let MDXComponent: (props: any) => JSX.Element;
-  export default MDXComponent;
-}
-
-declare module "*.md" {
-  let MDXComponent: (props: any) => JSX.Element;
-  export default MDXComponent;
-}
