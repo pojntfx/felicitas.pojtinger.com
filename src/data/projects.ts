ba1f6ff0c645eff401495ad8eaa048f03c7fc200
Felix Pojtinger <felix@pojtinger.com>
2020-01-05 13:52:01 +0100

    refactor: Re-organize projects so that the most relevant ones get more attention

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,85 +1,7 @@
 const projects = [
   {
-    title: "Websites",
-    icon: "newspaper",
-    projects: [
-      {
-        title: "Personal Site",
-        link: "/",
-        repoLink: "https://github.com/pojntfx/personal-site",
-        license: "AGPL-3.0",
-        date: "2019 (in development)",
-        description: "Central hub for my projects and articles.",
-        icon: "home",
-        color: "blue"
-      },
-      {
-        title: "Family Site",
-        link: "https://pojtinger.com/",
-        repoLink: "https://github.com/pojntfx/family-site",
-        license: "AGPL-3.0",
-        date: "2019-12-29",
-        description: "A minimalist site I built for my family.",
-        icon: "tree",
-        color: "green"
-      },
-      {
-        title: "RvWG",
-        link: "https://rvwg.de/home/",
-        repoLink: "https://github.com/pojntfx/rvwg-website",
-        license: "GPL-3.0",
-        date: "2017-06-01",
-        description: "Legacy website for my former high school (in German).",
-        icon: "university",
-        color: "orange"
-      },
-      {
-        title: "RvWG Neo",
-        link: "https://rvwg-neo.felix.pojtinger.com/",
-        repoLink: "https://github.com/pojntfx/rvwg-website-neo",
-        license: "AGPL-3.0",
-        date: "2018-11-06",
-        description:
-          "Partial rewrite of the RvWG site using the JAMStack (in German).",
-        icon: "space shuttle",
-        color: "yellow"
-      },
-      {
-        title: "Pension Café Bäckerei Züfle",
-        link: "https://cafe-zuefle.alphahorizon.io/",
-        repoLink:
-          "https://github.com/alphahorizonio/pension-cafe-baeckerei-zuefle-site",
-        license: "AGPL-3.0",
-        date: "2019-02-01",
-        description: "JAMStack site for a local business.",
-        icon: "food",
-        color: "orange"
-      },
-      {
-        title: "Gästehaus Gerhard Finkbeiner",
-        link: "https://fewo-finkbeiner.alphahorizon.io/",
-        repoLink: "https://github.com/alphahorizonio/fewo-finkbeiner-website",
-        license: "AGPL-3.0",
-        date: "2019-05-01",
-        description: "JAMStack site for a local B&B (in German).",
-        icon: "bed",
-        color: "pink"
-      },
-      {
-        title: "Ergotherapie Smeets-Labee",
-        link: "https://ergotherapie-ssl.de/",
-        repoLink: "https://github.com/alphahorizonio/ergotherapie-ssl-website",
-        license: "AGPL-3.0",
-        date: "2018-07",
-        description: "JAMStack site for a local doctor's office (in German).",
-        icon: "doctor",
-        color: "red"
-      }
-    ]
-  },
-  {
-    title: "Space and Systems",
-    icon: "rocket",
+    title: "DevOps",
+    icon: "server",
     projects: [
       {
         title: "Cluster Platform",
@@ -92,136 +14,6 @@ const projects = [
         icon: "users",
         color: "black"
       },
-      {
-        title: "LibreSat",
-        link: "https://libresat.clusterplatform.io/",
-        repoLink: "https://github.com/clusterplatform/libresat",
-        license: "AGPL-3.0",
-        date: "2018-11",
-        description:
-          "Full-stack satellite development and control platform (downstream of the Cluster Platform).",
-        icon: "globe",
-        color: "black"
-      },
-      {
-        title: "OpenSDCP",
-        link: "https://opensdcp.clusterplatform.io/",
-        repoLink: "https://github.com/clusterplatform/opensdcp-infrastructure",
-        license: "AGPL-3.0",
-        date: "2018-06",
-        description:
-          "Former satellite development and control platform (replaced by LibreSat).",
-        icon: "th",
-        color: "black"
-      },
-      {
-        title: "ModSat Standard",
-        link: "https://a360.co/2OkjFJq",
-        license: "AGPL-3.0",
-        date: "2016-10",
-        description: "Modular standard for nanosatellites.",
-        icon: "box",
-        color: "blue"
-      },
-      {
-        title: "ModSat Modules",
-        link: "https://pojntfx.lima-city.de/sites/modsat.html",
-        license: "AGPL-3.0",
-        description:
-          "Compute, communication, propulsion, energy and navigation modules for nanosatellites.",
-        date: "2016-10",
-        icon: "boxes",
-        color: "blue"
-      },
-      {
-        title: "ModSat Engine",
-        link: "https://a360.co/2VxN1ex",
-        license: "AGPL-3.0",
-        date: "2017-04",
-        description:
-          "Modular dual-stage 4-grid 3D-printed ion engine for nanosatellites.",
-        icon: "rocket",
-        color: "blue"
-      },
-      {
-        title: "ModSat Presentation",
-        link:
-          "https://drive.google.com/file/d/0B7GcA5vEfPYoNWJjRlFoeW9FaEE/view?usp=sharing",
-        license: "AGPL-3.0",
-        date: "2017-04",
-        description:
-          "Overview of the ModSat nanosatellite project (in German).",
-        icon: "slideshare",
-        color: "blue"
-      },
-      {
-        title: "ModSat Paper",
-        link:
-          "https://drive.google.com/file/d/0B7GcA5vEfPYoaFpGMkRxOWI1Unc/view?usp=sharing",
-        license: "CC-BY-SA 4.0",
-        date: "2016-08",
-        description:
-          "Detailed report on the ModSat nanosatellite project (in German).",
-        icon: "book",
-        color: "blue"
-      },
-      {
-        title: "SIEO",
-        link: "https://pojntfx.lima-city.de/sites/sieo.html",
-        license: "AGPL-3.0",
-        date: "2016-05",
-        description:
-          "Smart Ion Engine Orbiter, an ion-engine powered nanosatellite reference system.",
-        icon: "globe",
-        color: "teal"
-      },
-      {
-        title: "SIEO Paper",
-        link:
-          "https://drive.google.com/file/d/0B7GcA5vEfPYoXzlPc2pjTlV3NkU/view",
-        license: "CC-BY-SA 4.0",
-        date: "2016-01",
-        description: "A detailed report on the SIEO project (in German).",
-        icon: "book",
-        color: "teal"
-      },
-      {
-        title: "XGlide",
-        link:
-          "https://drive.google.com/file/d/0B7GcA5vEfPYoMWF4dE9MQTl5S1k/view?usp=sharing",
-        license: "AGPL-3.0",
-        date: "2016-06",
-        description:
-          "An ultra-lightweight CNC-milled blended-wing-body aircraft (in German).",
-        icon: "plane",
-        color: "red"
-      },
-      {
-        title: "Brahma",
-        link: "https://a360.co/2JkwLam",
-        license: "AGPL-3.0",
-        date: "2017-01",
-        description:
-          "A global surveillance and tracking system based on low-level components and shortwave radio.",
-        icon: "eye",
-        color: "black"
-      },
-      {
-        title: "Brahma Paper",
-        link:
-          "https://drive.google.com/file/d/0B7GcA5vEfPYoS0lIRThRcVJ5aEE/view?usp=sharing",
-        license: "CC-BY-SA 4.0",
-        date: "2017-01",
-        description: "A detailed report on the Brahma project (in German).",
-        icon: "book",
-        color: "black"
-      }
-    ]
-  },
-  {
-    title: "DevOps",
-    icon: "server",
-    projects: [
       {
         title: "Charts",
         link: "https://pojntfx.github.io/charts/",
@@ -321,6 +113,203 @@ const projects = [
           "A detailed report on both Illustrierte and Irasync (in German).",
         icon: "book",
         color: "teal"
+      }
+    ]
+  },
+  {
+    title: "Space",
+    icon: "rocket",
+    projects: [
+      {
+        title: "LibreSat",
+        link: "https://libresat.clusterplatform.io/",
+        repoLink: "https://github.com/clusterplatform/libresat",
+        license: "AGPL-3.0",
+        date: "2018-11",
+        description:
+          "Full-stack satellite development and control platform (downstream of the Cluster Platform).",
+        icon: "globe",
+        color: "black"
+      },
+      {
+        title: "OpenSDCP",
+        link: "https://opensdcp.clusterplatform.io/",
+        repoLink: "https://github.com/clusterplatform/opensdcp-infrastructure",
+        license: "AGPL-3.0",
+        date: "2018-06",
+        description:
+          "Former satellite development and control platform (replaced by LibreSat).",
+        icon: "th",
+        color: "black"
+      },
+      {
+        title: "ModSat Standard",
+        link: "https://a360.co/2OkjFJq",
+        license: "AGPL-3.0",
+        date: "2016-10",
+        description: "Modular standard for nanosatellites.",
+        icon: "box",
+        color: "blue"
+      },
+      {
+        title: "ModSat Modules",
+        link: "https://pojntfx.lima-city.de/sites/modsat.html",
+        license: "AGPL-3.0",
+        description:
+          "Compute, communication, propulsion, energy and navigation modules for nanosatellites.",
+        date: "2016-10",
+        icon: "boxes",
+        color: "blue"
+      },
+      {
+        title: "ModSat Engine",
+        link: "https://a360.co/2VxN1ex",
+        license: "AGPL-3.0",
+        date: "2017-04",
+        description:
+          "Modular dual-stage 4-grid 3D-printed ion engine for nanosatellites.",
+        icon: "rocket",
+        color: "blue"
+      },
+      {
+        title: "ModSat Presentation",
+        link:
+          "https://drive.google.com/file/d/0B7GcA5vEfPYoNWJjRlFoeW9FaEE/view?usp=sharing",
+        license: "AGPL-3.0",
+        date: "2017-04",
+        description:
+          "Overview of the ModSat nanosatellite project (in German).",
+        icon: "slideshare",
+        color: "blue"
+      },
+      {
+        title: "ModSat Paper",
+        link:
+          "https://drive.google.com/file/d/0B7GcA5vEfPYoaFpGMkRxOWI1Unc/view?usp=sharing",
+        license: "CC-BY-SA 4.0",
+        date: "2016-08",
+        description:
+          "Detailed report on the ModSat nanosatellite project (in German).",
+        icon: "book",
+        color: "blue"
+      },
+      {
+        title: "SIEO",
+        link: "https://pojntfx.lima-city.de/sites/sieo.html",
+        license: "AGPL-3.0",
+        date: "2016-05",
+        description:
+          "Smart Ion Engine Orbiter, an ion-engine powered nanosatellite reference system.",
+        icon: "globe",
+        color: "teal"
+      },
+      {
+        title: "SIEO Paper",
+        link:
+          "https://drive.google.com/file/d/0B7GcA5vEfPYoXzlPc2pjTlV3NkU/view",
+        license: "CC-BY-SA 4.0",
+        date: "2016-01",
+        description: "A detailed report on the SIEO project (in German).",
+        icon: "book",
+        color: "teal"
+      },
+      {
+        title: "Brahma",
+        link: "https://a360.co/2JkwLam",
+        license: "AGPL-3.0",
+        date: "2017-01",
+        description:
+          "A global surveillance and tracking system based on low-level components and shortwave radio.",
+        icon: "eye",
+        color: "black"
+      },
+      {
+        title: "Brahma Paper",
+        link:
+          "https://drive.google.com/file/d/0B7GcA5vEfPYoS0lIRThRcVJ5aEE/view?usp=sharing",
+        license: "CC-BY-SA 4.0",
+        date: "2017-01",
+        description: "A detailed report on the Brahma project (in German).",
+        icon: "book",
+        color: "black"
+      }
+    ]
+  },
+  {
+    title: "Websites",
+    icon: "newspaper",
+    projects: [
+      {
+        title: "Personal Site",
+        link: "/",
+        repoLink: "https://github.com/pojntfx/personal-site",
+        license: "AGPL-3.0",
+        date: "2019 (in development)",
+        description: "Central hub for my projects and articles.",
+        icon: "home",
+        color: "blue"
+      },
+      {
+        title: "Family Site",
+        link: "https://pojtinger.com/",
+        repoLink: "https://github.com/pojntfx/family-site",
+        license: "AGPL-3.0",
+        date: "2019-12-29",
+        description: "A minimalist site I built for my family.",
+        icon: "tree",
+        color: "green"
+      },
+      {
+        title: "RvWG",
+        link: "https://rvwg.de/home/",
+        repoLink: "https://github.com/pojntfx/rvwg-website",
+        license: "GPL-3.0",
+        date: "2017-06-01",
+        description: "Legacy website for my former high school (in German).",
+        icon: "university",
+        color: "orange"
+      },
+      {
+        title: "RvWG Neo",
+        link: "https://rvwg-neo.felix.pojtinger.com/",
+        repoLink: "https://github.com/pojntfx/rvwg-website-neo",
+        license: "AGPL-3.0",
+        date: "2018-11-06",
+        description:
+          "Partial rewrite of the RvWG site using the JAMStack (in German).",
+        icon: "space shuttle",
+        color: "yellow"
+      },
+      {
+        title: "Pension Café Bäckerei Züfle",
+        link: "https://cafe-zuefle.alphahorizon.io/",
+        repoLink:
+          "https://github.com/alphahorizonio/pension-cafe-baeckerei-zuefle-site",
+        license: "AGPL-3.0",
+        date: "2019-02-01",
+        description: "JAMStack site for a local business.",
+        icon: "food",
+        color: "orange"
+      },
+      {
+        title: "Gästehaus Gerhard Finkbeiner",
+        link: "https://fewo-finkbeiner.alphahorizon.io/",
+        repoLink: "https://github.com/alphahorizonio/fewo-finkbeiner-website",
+        license: "AGPL-3.0",
+        date: "2019-05-01",
+        description: "JAMStack site for a local B&B (in German).",
+        icon: "bed",
+        color: "pink"
+      },
+      {
+        title: "Ergotherapie Smeets-Labee",
+        link: "https://ergotherapie-ssl.de/",
+        repoLink: "https://github.com/alphahorizonio/ergotherapie-ssl-website",
+        license: "AGPL-3.0",
+        date: "2018-07",
+        description: "JAMStack site for a local doctor's office (in German).",
+        icon: "doctor",
+        color: "red"
       }
     ]
   },
