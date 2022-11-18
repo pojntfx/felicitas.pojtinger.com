9f6f0e408f7139f109a268026a780bf045889d18
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-11-18 21:56:36 +0100

    feat: Add Mastodon API endpoint

---
+++ api/mastodon/index.go
@@ -0,0 +1,127 @@
+package mastodon
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/mattn/go-mastodon"
+)
+
+type Output struct {
+	UserDisplayName       string `json:"userDisplayName"`
+	UserName              string `json:"username"`
+	UserFollowerCount     int    `json:"userFollowerCount"`
+	UserURL               string `json:"userURL"`
+	UserProfilePictureURL string `json:"userProfilePictureURL"`
+
+	Toots []Toot `json:"toos"`
+}
+
+type Toot struct {
+	Timestamp string `json:"timestamp"`
+	Body      string `json:"body"`
+
+	Images []Image `json:"images"`
+
+	RepliesCount   int `json:"repliesCount"`
+	ReblogsCount   int `json:"reblogsCount"`
+	FavoritesCount int `json:"favoritesCount"`
+
+	URL string `json:"url"`
+}
+
+type Image struct {
+	URL     string `json:"url"`
+	AltText string `json:"altText"`
+}
+
+func MastodonFeedHandler(w http.ResponseWriter, r *http.Request, server string, clientID string, clientSecret string, accessToken string) {
+	username := r.URL.Query().Get("username")
+	if username == "" {
+		w.Write([]byte("missing username query parameter: "))
+
+		panic("missing username query parameter")
+	}
+
+	client := mastodon.NewClient(&mastodon.Config{
+		Server:       server,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		AccessToken:  accessToken,
+	})
+
+	output := Output{}
+
+	accounts, err := client.AccountsSearch(r.Context(), username, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(accounts) != 1 {
+		w.WriteHeader(http.StatusNotFound)
+
+		panic(http.StatusNotFound)
+	}
+
+	account, err := client.GetAccount(r.Context(), accounts[0].ID)
+	if err != nil {
+		panic(err)
+	}
+
+	output.UserDisplayName = account.DisplayName
+	output.UserName = account.Username
+	output.UserFollowerCount = int(account.FollowersCount)
+	output.UserURL = account.URL
+	output.UserProfilePictureURL = account.AvatarStatic
+
+	toots := []Toot{}
+
+	sourceToots, err := client.GetAccountStatuses(r.Context(), account.ID, &mastodon.Pagination{
+		Limit: 5,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, sourceToot := range sourceToots {
+		toot := Toot{}
+
+		toot.Timestamp = sourceToot.CreatedAt.Format(time.RFC3339)
+		toot.Body = sourceToot.Content
+
+		images := []Image{}
+
+		for _, attachment := range sourceToot.MediaAttachments {
+			images = append(images, Image{
+				URL:     attachment.URL,
+				AltText: attachment.Description,
+			})
+		}
+
+		toot.Images = images
+
+		toot.RepliesCount = int(sourceToot.RepliesCount)
+		toot.ReblogsCount = int(sourceToot.ReblogsCount)
+		toot.FavoritesCount = int(sourceToot.FavouritesCount)
+
+		toot.URL = sourceToot.URL
+
+		toots = append(toots, toot)
+	}
+
+	output.Toots = toots
+
+	j, err := json.Marshal(output)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "%v", string(j))
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	MastodonFeedHandler(w, r, os.Getenv("MASTODON_SERVER"), os.Getenv("MASTODON_CLIENT_ID"), os.Getenv("MASTODON_CLIENT_SECRET"), os.Getenv("MASTODON_ACCESS_TOKEN"))
+}
