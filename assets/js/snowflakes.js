ef7170f2e11775c34a4ec8ce0a2686e0014ec7b3
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-11-28 02:23:50 +0100

    feat: Add `prefers-reduced-motion` handling support for winter edition snowflakes

--- assets/js/snowflakes.js
+++ assets/js/snowflakes.js
@@ -1,5 +1,20 @@
 import Snowflakes from "magic-snowflakes";
 
-new Snowflakes({
+const sv = new Snowflakes({
   color: "#ffffff",
 });
+
+const syncSnowflakes = (mq) => {
+  if (mq.matches) {
+    sv.stop();
+    sv.hide();
+  } else {
+    sv.show();
+    sv.start();
+  }
+};
+
+const mq = window.matchMedia("(prefers-reduced-motion: reduce)");
+
+syncSnowflakes(mq);
+mq.addEventListener("change", () => syncSnowflakes(mq));
