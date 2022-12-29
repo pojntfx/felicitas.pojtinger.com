1c4781b90b533ab365af7456020b66c993f12676
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-12-29 23:57:06 +0100

    fix: Add a11y support to fireworks

--- assets/js/fireworks.js
+++ assets/js/fireworks.js
@@ -1,1 +1,18 @@
 import "@fireworks-js/web";
+
+const syncFireworks = (mq) => {
+  if (mq.matches) {
+    document
+      .querySelectorAll(".fireworks")
+      .forEach((node) => node.fireworks.stop());
+  } else {
+    document
+      .querySelectorAll(".fireworks")
+      .forEach((node) => node.fireworks.start());
+  }
+};
+
+const mq = window.matchMedia("(prefers-reduced-motion: reduce)");
+
+syncFireworks(mq);
+mq.addEventListener("change", () => syncFireworks(mq));
